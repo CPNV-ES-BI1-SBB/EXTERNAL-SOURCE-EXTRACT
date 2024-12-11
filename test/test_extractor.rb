3e3054ffb7eb35469a2ae1451b86f91936978d34
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2024-12-11 16:41:59 +0100

    test: write test extract all data without exceptions

--- test/test_extractor.rb
+++ test/test_extractor.rb
@@ -1,7 +1,9 @@
+require 'json'
 require_relative 'test_helper'
 
 class TestExtractor < Minitest::Test
   def setup
+    # Given an API client, a logger
     @api_client = APIClient.new(base_url: 'https://api.example.com', headers: {}, timeout: 5)
     @logger = Logger.new(log_path: 'test_log.txt')
     @extractor = Extractor.new(api_client: @api_client, logger: @logger, max_retries: 3)
@@ -16,7 +18,19 @@ class TestExtractor < Minitest::Test
     assert_equal 3, @extractor.max_retries
   end
 
-  def test_extract_all_data_without_errors
-    # test
+  def test_extract_all_data_without_exceptions
+    # Given: No oldest_record, no missing data, no duplicates
+    mock_file_path = File.join(__dir__, 'mocks', 'mock_stationboard_lausanne_2024_12_01.json')
+    mock_response = JSON.parse(File.read(mock_file_path))
+
+    # When: Extracting data
+    @api_client.stub(:get, mock_response) do
+      result = @extractor.extract(oldest_record: {})
+
+      # Then: Should return a list of all records and log the result
+      assert_equal mock_response["trains"], result
+      @oldest_record = result.last
+      assert_includes @logger.last_log, "Extraction result: #{result}"
+    end
   end
 end
