358b78e5fc8770d1727d93a6f8c2f9637edfba60
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2024-12-13 20:16:57 +0100

    test: add test for handle missing data

--- test/test_extractor.rb
+++ test/test_extractor.rb
@@ -63,6 +63,37 @@ class TestExtractor < Minitest::Test
       assert_equal mock_response["connections"].first, @extractor.newest_record_received["connections"].first
     end
   end
+
+  def test_handle_missing_data_until_all_data_are_retrieved
+    # Given: Oldest_record, missing data, no duplicates
+    mock_file_path_missing = File.join(__dir__, 'mocks', 'mock_stationboard_lausanne_2024_12_01_missing_data.json')
+    mock_file_path_complete = File.join(__dir__, 'mocks', 'mock_stationboard_lausanne_2024_12_01.json')
+    mock_response_missing = JSON.parse(File.read(mock_file_path_missing))
+    mock_response_complete = JSON.parse(File.read(mock_file_path_complete))
+    call_count = 0
+
+      # Mock the API call to return the missing data first and then the complete data
+      @api_client.stub(:get, lambda {
+        call_count += 1
+        call_count == 1 ? mock_response_missing : mock_response_complete
+      }) do
+        # When: Extracting data
+        result = @extractor.extract(oldest_record_stored: @oldest_record_stored)
+        
+        # Then: Should fill the holes in the data and return a list of all records and log the result 
+        # Ensure mock_response_complete is returned
+        assert_equal mock_response_complete, result
+
+        # Ensure `handle_missing` is invoked and it recalls extract to get the complete data
+        assert_equal 2, call_count
+
+        # Ensure the variables are updated
+        assert_equal mock_response_complete, @extractor.current_data
+        assert_equal mock_response_complete["connections"].last, @extractor.oldest_record_stored["connections"].last
+        assert_equal mock_response_complete["connections"].first, @extractor.newest_record_received["connections"].first
+      end
+  end
+
     end
   end
 end
