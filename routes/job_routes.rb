cb91316e04077ba6911a76e14d00859238f98742
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2025-01-23 10:35:45 +0100

    feat: add a check if the data is already in the cache

--- routes/job_routes.rb
+++ routes/job_routes.rb
@@ -24,13 +24,21 @@ module JobRoutes
       settings.logger.info("Job created with ID: #{job_id}")
 
       begin
+        cached_data = settings.cache_manager.search_json_db(endpoint)
+          
+        if cached_data
+          settings.logger.info("Data already extracted for endpoint: #{endpoint}")
+          status 200
+          return { status: 'completed', url: settings.cache_manager.generate_signed_url(cached_data['uuid']) }.to_json
+        end
+        
         http_client = HTTPClient.new
         extractor = Extractor.new
-        result = extractor.extract(http_client: http_client, endpoint: endpoint)
+        result = extractor.extract(http_client: http_client, endpoint: endpoint)      
 
         settings.logger.info("Data extraction completed for job ID: #{job_id}")
         
-        status 201
+        status 200
         { status: 'completed', url: settings.cache_manager.upload_json_data(job_id, endpoint, result) }.to_json
 
       rescue Extractor::MaxRetriesReachedError => e
